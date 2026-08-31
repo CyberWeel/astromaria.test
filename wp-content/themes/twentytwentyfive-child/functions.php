bf6e44cedb91dfed6e21c3bc6703cf00ff997f53
CyberWeel <47432522+CyberWeel@users.noreply.github.com>
2026-08-31 23:29:52 +0300

    Создана дочерняя тема

---
+++ wp-content/themes/twentytwentyfive-child/functions.php
@@ -0,0 +1,5 @@
+<?
+add_action('wp_enqueue_scripts', 'my_child_theme_enqueue_styles');
+function my_child_theme_enqueue_styles() {
+  wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
+}
