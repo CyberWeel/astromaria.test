069bc6733132d76db8a662712eebc0db3d12b9f4
CyberWeel <47432522+CyberWeel@users.noreply.github.com>
2026-09-01 00:54:31 +0300

    Удалена ненужная тема

--- wp-content/themes/twentytwentyfive-child/functions.php
+++
@@ -1,5 +0,0 @@
-<?
-add_action('wp_enqueue_scripts', 'my_child_theme_enqueue_styles');
-function my_child_theme_enqueue_styles() {
-  wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
-}
