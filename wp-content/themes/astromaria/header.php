90c6fb5747b6e1ae5e4669e9e713186a5998c2f3
CyberWeel <47432522+CyberWeel@users.noreply.github.com>
2026-09-05 02:51:07 +0300

    Шапка внесена, функции SCSS удалены, небольшие изменения SCSS

--- wp-content/themes/astromaria/header.php
+++ wp-content/themes/astromaria/header.php
@@ -11,12 +11,22 @@
         <header class="header">
           <div class="container">
             <div class="header__content">
-              <!--  -->
-              111
+              <a class="header__logo" href="/">
+                <img class="header__logo-link" src="/wp-content/themes/astromaria/images/logo-image.png" alt="Логотип">
+                <span class="header__logo-text">AstroMaria</span>
+              </a>
+              <nav class="header__menu">
+                <a class="header__menu-link" href="/news/">Новости</a>
+                <a class="header__menu-link" href="/articles/">Статьи</a>
+                <a class="header__menu-link" href="/contacts/">Контакты</a>
+                <a class="header__menu-link" href="/about/">О нас</a>
+              </nav>
+              <div class="header__search">
+                <a class="header__search-link" href="#"></a>
+              </div>
             </div>
           </div>
         </header>
         <main class="site__main">
           <div class="container">
             <div class="site__main-content">
-              <!--  -->
