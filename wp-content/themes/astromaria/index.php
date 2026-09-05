476c9fb08c2758d8fc3103c5b9c4085c19e3a869
CyberWeel <47432522+CyberWeel@users.noreply.github.com>
2026-09-05 03:06:02 +0300

    Добавлен баннер на главную страницу

--- wp-content/themes/astromaria/index.php
+++ wp-content/themes/astromaria/index.php
@@ -1,3 +1,30 @@
 <? get_header() ?>
+<section class="banner">
+  <div class="banner__content">
+    <h1 class="banner__heading">Откройте Вселенную вместе с <span class="banner__heading-colored">AstroMaria</span></h1>
+    <div class="banner__description">Научно-популярные материалы об астрономии, космосе и последних открытиях.</div>
+    <a class="banner__button" href="/articles/">Читать статьи <span>&#8594</span></a>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <? get_footer() ?>
