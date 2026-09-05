c0498698d9048f015e7a8fb9a013dd5902c8c7ae
CyberWeel <47432522+CyberWeel@users.noreply.github.com>
2026-09-05 15:09:57 +0300

    Футер добавлен

--- wp-content/themes/astromaria/footer.php
+++ wp-content/themes/astromaria/footer.php
@@ -3,10 +3,42 @@
         </main>
       </div>
       <footer class="footer">
-        <div class="container">
+        <div class="container footer__outer">
           <div class="footer__content">
-            <!--  -->
+            <div class="footer__main">
+              <a class="footer__logo" href="/">
+                <img class="footer__logo-link" src="/wp-content/themes/astromaria/images/logo-image.png" alt="Логотип">
+                <span class="footer__logo-text">AstroMaria</span>
+              </a>
+              <div class="footer__main-text">AstroMaria - ваш проводник в мире астрономии и космоса. Мы делимся знаниями, вдохновляем и открываем Вселенную вместе с вами.</div>
+            </div>
+            <div class="footer__menu">
+              <div class="footer__menu-heading">Навигация</div>
+              <div class="footer__menu-list">
+                <a class="footer__menu-link" href="/news/">Новости</a>
+                <a class="footer__menu-link" href="/articles/">Статьи</a>
+                <a class="footer__menu-link" href="/contacts/">Контакты</a>
+                <a class="footer__menu-link" href="/about/">О нас</a>
+              </div>
+            </div>
+            <div class="footer__socials">
+              <div class="footer__socials-heading">Мы в соцсетях</div>
+              <div class="footer__socials-list">
+                <div class="footer__socials-item">
+                  <a class="footer__socials-link footer__socials-link--vk" href="https://vk.ru/astromaria_assistant" target="_blank"></a>
+                </div>
+              </div>
+            </div>
+            <div class="footer__contacts">
+              <div class="footer__contacts-heading">Контакты</div>
+              <div class="footer__contacts-list">
+                <a class="footer__contacts-link" href="mailto:info@astromaria.ru">info@astromaria.ru</a>
+                <a class="footer__contacts-link" href="tel:+79991234567">+7 (999) 123-45-67</a>
+                <div class="footer__contacts-address">Россия, г. Москва</div>
+              </div>
+            </div>
           </div>
+          <div class="footer__copyright">© 2026 AstroMaria. Все права защищены.</div>
         </div>
       </footer>
     </div>
