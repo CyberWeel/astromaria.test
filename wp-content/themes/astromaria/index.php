d4ae8ce79eaf9d6b9f69520876e8a537fd12054e
CyberWeel <47432522+CyberWeel@users.noreply.github.com>
2026-09-12 17:20:30 +0300

    "Последние новости" и "Последние статьи" имеют одинаковые CSS

--- wp-content/themes/astromaria/index.php
+++ wp-content/themes/astromaria/index.php
@@ -11,20 +11,20 @@ $last_articles = get_posts(['posts_per_page' => 4]);
     <a class="banner__button" href="/articles/">Читать статьи <span>&#8594</span></a>
   </div>
 </section>
-<section class="last-news">
-  <div class="last-news__header">
-    <h2 class="last-news__heading">Последние новости</h2>
-    <a class="last-news__header-link" href="/news/">Смотреть все новости &#8594</a>
+<section class="last-articles">
+  <div class="last-articles__header">
+    <h2 class="last-articles__heading">Последние новости</h2>
+    <a class="last-articles__header-link" href="/news/">Смотреть все новости &#8594</a>
   </div>
-  <div class="last-news__content"><?
+  <div class="last-articles__content"><?
     foreach ($last_news as $last_news_item) {
       setup_postdata($post); ?>
-      <a class="last-news__item" href="<?=get_permalink($last_news_item->ID)?>">
-        <div class="last-news__image" style="background-image: url('<?=get_the_post_thumbnail_url($last_news_item->ID)?>');"></div>
-        <div class="last-news__item-content">
-          <div class="last-news__date"><?=wp_date('j F Y', strtotime($last_news_item->post_date))?></div>
-          <div class="last-news__name"><?=$last_news_item->post_title?></div>
-          <div class="last-news__description"><?=show_custom_excerpt($last_news_item->post_content)?></div>
+      <a class="last-articles__item" href="<?=get_permalink($last_news_item->ID)?>">
+        <div class="last-articles__image" style="background-image: url('<?=get_the_post_thumbnail_url($last_news_item->ID)?>');"></div>
+        <div class="last-articles__item-content">
+          <div class="last-articles__date"><?=wp_date('j F Y', strtotime($last_news_item->post_date))?></div>
+          <div class="last-articles__name"><?=$last_news_item->post_title?></div>
+          <div class="last-articles__description"><?=show_custom_excerpt($last_news_item->post_content)?></div>
         </div>
       </a><?
     }
