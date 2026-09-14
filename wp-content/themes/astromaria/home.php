e127a3b20a86642f3c794cd088bc25970aeb6d2e
CyberWeel <47432522+CyberWeel@users.noreply.github.com>
2026-09-14 08:49:05 +0300

    Общие страницы статей и новостей

--- wp-content/themes/astromaria/home.php
+++ wp-content/themes/astromaria/home.php
@@ -1,62 +1,29 @@
-<?php # Временное наполнение
-
-get_header(); ?>
-
-<?php
-if (function_exists('yoast_breadcrumb')) {
-    yoast_breadcrumb('<div class="breadcrumbs">', '</div>');
-}
+<? get_header();
+yoast_breadcrumb('<div class="breadcrumbs">', '</div>');
 ?>
+<section class="articles articles--list">
+  <h1 class="articles__heading">Статьи</h1>
 
-<h1>Новости</h1>
+  <? if (have_posts()) { ?>
+    <div class="articles__content"><?
+      while (have_posts()) {
+        the_post(); ?>
+        <a class="articles__item" href="<? the_permalink() ?>">
+          <div class="articles__image" style="background-image: url('<?=get_the_post_thumbnail_url(get_the_ID())?>');"></div>
+          <div class="articles__item-content">
+            <div class="articles__date"><?=wp_date('j F Y', strtotime(get_the_date()))?></div>
+            <div class="articles__name"><? the_title() ?></div>
+            <div class="articles__description"><?=show_custom_excerpt(get_the_excerpt())?></div>
+          </div>
+        </a>
+      <? } ?>
+    </div><?
 
-<?php if (have_posts()) : ?>
-
-    <div class="news-list">
-
-        <?php while (have_posts()) : the_post(); ?>
-
-            <article class="news-item">
-
-                <?php if (has_post_thumbnail()) : ?>
-                    <a href="<?php the_permalink(); ?>">
-                        <?php the_post_thumbnail('medium'); ?>
-                    </a>
-                <?php endif; ?>
-
-                <div>
-                    <time datetime="<?php echo esc_attr(get_the_date('c')); ?>">
-                        <?php echo esc_html(get_the_date('d.m.Y')); ?>
-                    </time>
-
-                    <h2>
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_title(); ?>
-                        </a>
-                    </h2>
-
-                    <?php the_excerpt(); ?>
-
-                </div>
-
-            </article>
-
-        <?php endwhile; ?>
-
-    </div>
-
-    <?php
     the_posts_pagination([
-        'mid_size'  => 2,
-        'prev_text' => '← Назад',
-        'next_text' => 'Вперёд →',
+      'mid_size'  => 2,
+      'prev_text' => '← Назад',
+      'next_text' => 'Вперёд →',
     ]);
-    ?>
-
-<?php else : ?>
-
-    <p>Статей пока нет.</p>
-
-<?php endif; ?>
-
-<?php get_footer(); ?>
+  } ?>
+</section>
+<? get_footer() ?>
