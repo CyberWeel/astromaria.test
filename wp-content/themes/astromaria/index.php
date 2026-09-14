6d23ec2f447836b62c244029fd233b64c46087e6
CyberWeel <47432522+CyberWeel@users.noreply.github.com>
2026-09-14 05:46:38 +0300

    last-articles become articles

--- wp-content/themes/astromaria/index.php
+++ wp-content/themes/astromaria/index.php
@@ -11,20 +11,20 @@ $last_articles = get_posts(['posts_per_page' => 4]);
     <a class="banner__button" href="/articles/">Читать статьи <span>&#8594</span></a>
   </div>
 </section>
-<section class="last-articles">
-  <div class="last-articles__header">
-    <h2 class="last-articles__heading">Последние новости</h2>
-    <a class="last-articles__header-link" href="/news/">Смотреть все новости &#8594</a>
+<section class="articles">
+  <div class="articles__header">
+    <h2 class="articles__heading">Последние новости</h2>
+    <a class="articles__header-link" href="/news/">Смотреть все новости &#8594</a>
   </div>
-  <div class="last-articles__content"><?
+  <div class="articles__content"><?
     foreach ($last_news as $last_news_item) {
       setup_postdata($post); ?>
-      <a class="last-articles__item" href="<?=get_permalink($last_news_item->ID)?>">
-        <div class="last-articles__image" style="background-image: url('<?=get_the_post_thumbnail_url($last_news_item->ID)?>');"></div>
-        <div class="last-articles__item-content">
-          <div class="last-articles__date"><?=wp_date('j F Y', strtotime($last_news_item->post_date))?></div>
-          <div class="last-articles__name"><?=$last_news_item->post_title?></div>
-          <div class="last-articles__description"><?=show_custom_excerpt($last_news_item->post_content)?></div>
+      <a class="articles__item" href="<?=get_permalink($last_news_item->ID)?>">
+        <div class="articles__image" style="background-image: url('<?=get_the_post_thumbnail_url($last_news_item->ID)?>');"></div>
+        <div class="articles__item-content">
+          <div class="articles__date"><?=wp_date('j F Y', strtotime($last_news_item->post_date))?></div>
+          <div class="articles__name"><?=$last_news_item->post_title?></div>
+          <div class="articles__description"><?=show_custom_excerpt($last_news_item->post_content)?></div>
         </div>
       </a><?
     }
@@ -32,19 +32,19 @@ $last_articles = get_posts(['posts_per_page' => 4]);
     wp_reset_postdata(); ?>
   </div>
 </section>
-<section class="last-articles">
-  <div class="last-articles__header">
-    <h2 class="last-articles__heading">Популярные статьи</h2>
-    <a class="last-articles__header-link" href="/articles/">Смотреть все статьи &#8594</a>
+<section class="articles">
+  <div class="articles__header">
+    <h2 class="articles__heading">Популярные статьи</h2>
+    <a class="articles__header-link" href="/articles/">Смотреть все статьи &#8594</a>
   </div>
-  <div class="last-articles__content"><?
+  <div class="articles__content"><?
     foreach ($last_articles as $last_articles_item) {
       setup_postdata($post); ?>
-      <a class="last-articles__item" href="<?=get_permalink($last_articles_item->ID)?>">
-        <div class="last-articles__image" style="background-image: url('<?=get_the_post_thumbnail_url($last_articles_item->ID)?>');"></div>
-        <div class="last-articles__item-content">
-          <div class="last-articles__name"><?=$last_articles_item->post_title?></div>
-          <div class="last-articles__description"><?=show_custom_excerpt($last_articles_item->post_content)?></div>
+      <a class="articles__item" href="<?=get_permalink($last_articles_item->ID)?>">
+        <div class="articles__image" style="background-image: url('<?=get_the_post_thumbnail_url($last_articles_item->ID)?>');"></div>
+        <div class="articles__item-content">
+          <div class="articles__name"><?=$last_articles_item->post_title?></div>
+          <div class="articles__description"><?=show_custom_excerpt($last_articles_item->post_content)?></div>
         </div>
       </a><?
     }
